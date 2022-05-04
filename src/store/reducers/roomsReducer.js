05a517a96f93289f7596ad6f79aee16ad702af4f
AlexDeng <a.denisov@elesta.ru>
2022-05-04 11:58:10 +0300

    final deploy

--- src/store/reducers/roomsReducer.js
+++ src/store/reducers/roomsReducer.js
@@ -4,19 +4,19 @@ const initialState = {};
 
 export default function roomsReducer(state = initialState, { type, payload }) {
   switch (type) {
-    case ACTION_TYPES.GET_ROOMS_SUCCESS:
-      return { ...state, ...payload };
-    case ACTION_TYPES.UPDATE_ROOM_SUCCESS:
-      return {
-        ...state,
-        [payload.id]: {
-          ...state[payload.id],
-          ...payload.data,
-        },
-      };
-    case ACTION_TYPES.LOGOUT:
-      return initialState;
-    default:
-      return state;
+  case ACTION_TYPES.GET_ROOMS_SUCCESS:
+    return { ...state, ...payload };
+  case ACTION_TYPES.UPDATE_ROOM_SUCCESS:
+    return {
+      ...state,
+      [payload.id]: {
+        ...state[payload.id],
+        ...payload.data,
+      },
+    };
+  case ACTION_TYPES.LOGOUT:
+    return initialState;
+  default:
+    return state;
   }
 }
